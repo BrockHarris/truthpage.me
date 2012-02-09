fce91041ab15238aa827e0396d6b7be7aa84b7bf
mikedelgrosso <mikedelgrosso@gmail.com>
2012-02-08 22:27:15 -0500

    More conflict resolutions with master and omniauth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
- 
   protect_from_forgery
-  
-  include SessionsHelper
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  #TODO: create some more robust authorization helpers or implement cancan
+  def require_admin
+    redirect_to root_path unless current_user.try(:admin)
+  end
 end
