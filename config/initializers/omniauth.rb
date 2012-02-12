3c7b97a9342154773a108246ad11438a1143b2d6
Brock <btharris781@gmail.com>
2012-02-12 01:57:19 -0500

    fixed more css

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '186414394781001', 'f6ab9f3c9f20412ac03c56f4abff88a1'
+  provider :facebook, '186414394781001', 'f6ab9f3c9f20412ac03c56f4abff88a1', :scope => 
+  
   provider :twitter, 'akOOojiNsQRXyKW3IrINxA', 'dgZ09rZC7c0WJfXF45GtLX23f1I5OQnOa4k5JNe538'
   provider :google_oauth2, '892785099521.apps.googleusercontent.com', 'pJCmfpRSdGzmTK4kL2QOJ_ae', {access_type: 'online', approval_prompt: ''}
   provider :identity, on_failed_registration: lambda { |env|    
