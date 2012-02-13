8feceacff2a51018d6cb93beab33159693923c0e
mikedelgrosso <mikedelgrosso@gmail.com>
2012-02-13 15:45:55 -0500

    Moved micropost form to views, refactored avatar image to helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   #def javascript(*args)
   #  content_for(:head) { javascript_include_tag(*args) }
   #end
+
+  def render_user_avatar(user, size=:small, opts={})
+    if user.photo.exists?
+      return image_tag user.photo.url(size)
+    else
+      image_tag "genericpic-#{size}.jpg" 
+    end
+  end
 end
