aea3958a4d42b9d88deb47a66a190aab12ea7ad4
Brock <btharris781@gmail.com>
2012-02-21 00:20:09 -0500

    possible jquery fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,9 @@ Truthpage::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #enabled true for heroku#
+  
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
