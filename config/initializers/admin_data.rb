bfcb08119f97337a6d90c99e9f0cd30256c0b6d7
Brock <btharris781@gmail.com>
2012-02-20 19:03:10 -0500

    change in admin_data

---
+++ config/initializers/admin_data.rb
@@ -0,0 +1,4 @@
+AdminData.config do |config|
+  config.is_allowed_to_view = lambda {|controller| return true if current_user.admin }
+  config.is_allowed_to_update = lambda {|controller| return true if current_user.admin }
+end
