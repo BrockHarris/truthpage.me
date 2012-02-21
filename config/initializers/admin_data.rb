1fddaf62a4a8fbdc03e92d90d5d1e8ce88bb1ba7
Brock <btharris781@gmail.com>
2012-02-20 19:45:57 -0500

    update assets

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,4 +1,4 @@
 AdminData.config do |config|
-  config.is_allowed_to_view = lambda {|controller| return true if current_user.admin = true }
-  config.is_allowed_to_update = lambda {|controller| return true if current_user.admin = true }
+  config.is_allowed_to_view = lambda {|controller|  if current_user.admin = true }
+  config.is_allowed_to_update = lambda {|controller| if current_user.admin = true }
 end
