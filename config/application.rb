55e2d7460f6d313b80bc30ebb468650a5459e599
Brock <btharris781@gmail.com>
2012-02-21 23:19:15 -0500

    added config assets

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Truthpage
     config.filter_parameters += [:password]
     config.assets.enabled = true
     config.assets.precompile += ['admin_data.css', 'admin_data.js']
+    config.assets.initialize_on_precompile = false
     config.assets.version = '1.0'
     config.autoload_paths += %W(#{config.root}/lib)
     config.middleware.use "NoWww"
