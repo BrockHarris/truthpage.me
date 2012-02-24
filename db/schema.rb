46e40e8a0a30585088ff36d76e781869f47753be
mikedelgrosso <mikedelgrosso@gmail.com>
2012-02-23 21:52:45 -0500

    Working model of destroying users without removing records.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120217173216) do
+ActiveRecord::Schema.define(:version => 20120224015656) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120217173216) do
     t.integer  "belongs_to_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
   end
 
   add_index "microposts", ["user_id", "profile_id", "created_at"], :name => "index_microposts_on_user_id_and_profile_id_and_created_at"
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20120217173216) do
     t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
   end
 
   add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
@@ -71,6 +73,7 @@ ActiveRecord::Schema.define(:version => 20120217173216) do
     t.integer  "photo_file_size"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
