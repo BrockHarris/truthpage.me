65f0487afeb7a6f41c84f26034f13b8efeb0a19b
Brock <btharris781@gmail.com>
2012-02-29 00:30:48 -0500

    started fancy box

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,19 +1,20 @@
 class MicropostsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-
+  before_filter :login_required
+  
   def create
-    @micropost = current_user.microposts.build(params[:micropost])
-    @user = User.find_by_username(params[:id])
-    if @micropost.save
-      #MicropostMailer.post_email(@user).deliver
-      flash[:notice] = "Your truth has been sent!"
-      redirect_to(:back)
-    else
-      @feed_items = []
-      flash[:alert] = "You haven't written anything!"
-      redirect_to(:back)
-    end
-  end
+     @micropost = current_user.microposts.build(params[:micropost])
+     @user = User.find_by_username(params[:id])
+     if @micropost.save
+       #MicropostMailer.post_email(@user).deliver
+       flash[:notice] = "Your truth has been sent!"
+       redirect_to(:back)
+     else
+       @feed_items = []
+       flash[:alert] = "You haven't written anything!"
+       redirect_to(:back)
+     end
+   end
  
   def destroy
     Micropost.find(params[:id]).destroy
