6aa6a76dc925d6a73bf0c05fcdaf2240eaef3a3d
Chris Scutti <chris.scutti@gmail.com>
2012-03-01 16:31:52 -0500

    chris's changes

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,6 @@ class MicropostsController < ApplicationController
      @micropost = current_user.microposts.build(params[:micropost])
      @user = User.find_by_username(params[:id])
      if @micropost.save
-       #MicropostMailer.post_email(@user).deliver
        flash[:notice] = "Your truth has been sent!"
        redirect_to(:back)
      else
@@ -14,6 +13,10 @@ class MicropostsController < ApplicationController
        flash[:alert] = "You haven't written anything!"
        redirect_to(:back)
      end
+   else
+     @micropost.save
+     # MicropostMailer will send out the email upon Micropost creation.
+     MicropostMailer.micropost_email(@user).deliver
    end
  
   def destroy
--- app/mailers/micropost_mailer.rb
+++ app/mailers/micropost_mailer.rb
@@ -2,9 +2,9 @@ class MicropostMailer < ActionMailer::Base
   default from: "support@truthpage.me"
   
   def post_email(micropost)
-    @micropost = Micropost
+    @micropost = micropost
     @user = User.find_by_username(params[:id])
     @url  = "http://truthpage.me/signup"
-    mail(:to => @user.email, :subject => "You have a new truth!")
+    mail(:to => @user.email, :subject => "You have a new truth on truthpage.me!")
   end
 end
