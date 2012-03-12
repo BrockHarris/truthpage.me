37d4f56b48bf83518d9677f073b24216bcbe204f
Chris Scutti <chris.scutti@gmail.com>
2012-03-11 22:42:28 -0400

    updated micropost_mailer

--- db/migrate/20120224015655_add_deleted_at_to_users_and_dependents.rb
+++
@@ -1,7 +0,0 @@
-class AddDeletedAtToUsersAndDependents < ActiveRecord::Migration
-  def change
-    add_column :users, :deleted_at, :datetime
-    add_column :microposts, :deleted_at, :datetime
-    add_column :relationships, :deleted_at, :datetime
-  end
-end
