567c3625a0b2f7e21042dfb98c3b6c13c98c404d
Brock <btharris781@gmail.com>
2012-03-20 13:26:05 -0400

    Added a few stats to the admin page.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,13 @@
 class Admin::UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+
   before_filter :admin_login_required
   before_filter :find_user, :except=>[:index]
 
   def index
-    @skip_render = true
+    @facebook_users = User.all(:conditions => "facebook_id IS NOT NULL",)
     @users = User.order('created_at DESC').all.paginate(:page => params[:page], :per_page => 40)
     @microposts  = Micropost.all
-    
+    @title = "Truthpage | Admin"
   end
 
   def destroy
