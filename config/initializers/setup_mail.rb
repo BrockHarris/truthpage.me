3d4a2f8a4884edfb5a1f6629a793b75d9c8f10f8
Brock <btharris781@gmail.com>
2012-03-21 23:32:42 -0400

    layout for auth home.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,  
   :domain               => "truthpage.me",  
   :user_name            => "support@truthpage.me",  
-  :password             => "Truthpage1",  
+  :password             => "truthpage1",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }  
