d8715a715ad63a2d2b79e5e5fc04a03aca39cd03
Brock <btharris781@gmail.com>
2012-04-18 16:33:39 -0400

    default scope on comments order.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ActiveRecord::Base
 	
 	after_create :create_notification
 	
+	default_scope order("comments.created_at ASC")
+
 	def create_notification
 		unless self.user_id==self.owner_id
     	notification = Notification.create!(:sender_id=>self.user_id, :receiver_id=>self.owner_id, :format=>"commented on a truth about you.", :micropost_content=>self.micropost_content, :comment=>self.post_comment)
