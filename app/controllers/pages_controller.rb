17d34de785c9e19fe4a389723957bf8398cc1eab
Brock <btharris781@gmail.com>
2012-04-20 13:50:46 -0400

    small changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 skip_before_filter :verify_authenticity_token
   def home
-    @faces = User.all(:limit => 16)
+    @faces = User.order('created_at DESC').all(:limit => 9)
     @users = User.all
     @title = "Truthpage.me"
     if current_user
